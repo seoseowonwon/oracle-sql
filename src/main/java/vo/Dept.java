7d1b07f568cc49c9b4debaa3603b2db43c8ed55f
Kiwon <mrkiwonseo@gmail.com>
2024-04-18 10:15:37 +0900

    dept, emp 테이블용 VO생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,10 @@
+package vo;
+
+//Value Object > DTO(Date Transfer Object) , Domain
+
+public class Dept {
+	public int deptNo;
+	public String dname;
+	public String loc;
+	
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,14 @@
+package vo;
+
+public class Emp {
+	
+	public int empno;
+	public String ename;
+	public String job;
+	public int mgr;
+	public String hiredate; 
+	public double sal;
+	public double comm;
+	public int deptno;
+	
+}
